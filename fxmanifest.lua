382e18d21ad3eec69f91447cb8247d3b8d5b8646
Souvik710 <93484649+Souvik710@users.noreply.github.com>
2026-06-17 09:14:33 +0530

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,35 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Ragna'
+description 'IMRP Apartments System - QBox Framework'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'shared/utils.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+
+files {
+    'locales/*.lua'
+}
+
+dependencies {
+    'ox_lib',
+    'oxmysql',
+    'ox_inventory',
+    'qbx_core',
+    'illenium-appearance'
+}
+
+lua54 'yes'
